cd00114d4c8fa7abbc2c003a89cdd333392cb47b
sarthakr10 <rastogisarthak35@gmail.com>
2024-08-03 05:11:56 +0530

    new

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -17,7 +17,7 @@ export const isAdminAuthenticated = catchAsyncErrors(
       req.user = await User.findById(decoded.id);
       if (req.user.role !== "Admin") {
         return next(
-          new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
+          new Errorhandler(`${req.user.role} not authorized for this resource!`, 403)
         );
       }
       next();
@@ -25,15 +25,33 @@ export const isAdminAuthenticated = catchAsyncErrors(
   );
   
 
-export const isPatientAuthenticated = catchAsyncErrors(async(req,res,next)=>{
-    const token = req.cookies.patientToken;
-    if(!token){
-        return next(new Errorhandler("Patient Not Authenticated!",400))
+// Middleware to authenticate frontend users
+export const isPatientAuthenticated = catchAsyncErrors(
+    async (req, res, next) => {
+      const token = req.cookies.patientToken;
+      if (!token) {
+        return next(new Errorhandler("User is not authenticated!", 400));
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      req.user = await User.findById(decoded.id);
+      if (req.user.role !== "Patient") {
+        return next(
+          new Errorhandler(`${req.user.role} not authorized for this resource!`, 403)
+        );
+      }
+      next();
     }
-    const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY);
-    req.user = await User.findById(decoded.id);
-    if (req.user.role !== "Patient") {
-        return next(new Errorhandler(`${req.user.role} not Authenticated for this resources!`,403))
-    }
-    next();
-})
+  );
+  
+  export const isAuthorized = (...roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return next(
+          new Errorhandler(
+            `${req.user.role} not allowed to access this resource!`
+          )
+        );
+      }
+      next();
+    };
+  };
