6f1160b3f5d7430ed0885d53ad760f586363dcaa
sarthakr10 <rastogisarthak35@gmail.com>
2024-09-07 00:58:31 +0530

    new

--- routes/messageRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { sendMessage } from "../controllers/messageController.js";
-
-const router = express.Router();
-router.post("/send",sendMessage);
-
-export default router;
