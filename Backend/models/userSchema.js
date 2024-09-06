297014c74907aac4bdb2d7a4b445313121b65e35
sarthakr10 <rastogisarthak35@gmail.com>
2024-09-07 02:32:10 +0530

    new

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -22,14 +22,14 @@ const userSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: [true, "Phone Is Required!"],
-    minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
-    maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+    minLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
+    maxLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
   },
   nic: {
     type: String,
-    required: [true, "NIC Is Required!"],
-    minLength: [13, "NIC Must Contain Only 13 Digits!"],
-    maxLength: [13, "NIC Must Contain Only 13 Digits!"],
+    required: [true, "Aadhaar Is Required!"],
+    minLength: [12, "Aadhaar Must Contain Only 12 Digits!"],
+    maxLength: [12, "Aadhaar Must Contain Only 12 Digits!"],
   },
   dob: {
     type: Date,
